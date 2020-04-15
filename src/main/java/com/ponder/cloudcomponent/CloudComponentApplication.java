bc26a0ce6dbd364bbcbdf9ce93d01206d9a963b0
gponder <gponder.g@gmail.com>
2020-04-15 20:00:04 +0800

    MapperScan

--- src/main/java/com/ponder/cloudcomponent/CloudComponentApplication.java
+++ src/main/java/com/ponder/cloudcomponent/CloudComponentApplication.java
@@ -1,9 +1,11 @@
 package com.ponder.cloudcomponent;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ponder.cloudcomponent.dao")
 public class CloudComponentApplication {
 
     public static void main(String[] args) {
