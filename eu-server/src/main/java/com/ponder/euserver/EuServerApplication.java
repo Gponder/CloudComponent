295479558ecfad34243cc3eacc3324e6ec04a7ad
gponder <gponder.g@gmail.com>
2020-04-20 17:57:11 +0800

    eureka server

--- eu-server/src/main/java/com/ponder/euserver/EuServerApplication.java
+++ eu-server/src/main/java/com/ponder/euserver/EuServerApplication.java
@@ -2,8 +2,10 @@ package com.ponder.euserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EuServerApplication {
 
     public static void main(String[] args) {
