91863c1bf3bb89775305e97994d1713189292689
gponder <gponder.g@gmail.com>
2020-04-28 15:51:47 +0800

    technology

---
+++ technology/src/main/java/com/ponder/technology/proxy/AOPInterface.java
@@ -0,0 +1,10 @@
+package com.ponder.technology.proxy;
+
+/**
+ * @auth ponder
+ * @Email gponder.g@gmail.com
+ * @create 2020/4/28 15:50
+ */
+public class AOPInterface {
+
+}
---
+++ technology/src/test/java/com/ponder/technology/TechnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ponder.technology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
